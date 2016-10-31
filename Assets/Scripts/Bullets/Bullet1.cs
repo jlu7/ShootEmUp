5ebf225d67fdf5b83abd413267fb9964703d3e27
jlu7 <tontonrue@gmail.com>
2016-10-30 22:38:34 -0700

    Ship can fly in 3d space and shoots in 3d space

---
+++ Assets/Scripts/Bullets/Bullet1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet1 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Logic());
+	}
+	
+	// Update is called once per frame
+	public IEnumerator Logic ()
+    {
+        while (true)
+        {
+            transform.position += new Vector3(0, .1f, 0);
+            if (transform.position.y > 10)
+            {
+                Destroy(this);
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveWithClick.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWithClick : MonoBehaviour
+{
+    float Speed = 5f;
+    Vector3 Target = new Vector3();
+    int CoolDownCount = 20;
+
+    void Start()
+    {
+        Target = transform.position;
+        StartCoroutine(Movement());
+        StartCoroutine(FireBullet());
+    }
+
+    protected IEnumerator Movement()
+    {
+        while (true)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0))
+            {
+                Target = hit.point;
+            }
+            transform.position = Vector3.Lerp(transform.position, new Vector3(Target.x, Target.y, transform.position.z), Speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    protected IEnumerator FireBullet()
+    {
+        int coolDown = CoolDownCount;
+        while(true)
+        {
+            if (coolDown <= 0 && Input.GetMouseButton(0))
+            {
+                coolDown = CoolDownCount;
+                Debug.Log("Shoot");
+                GameObject blah = Instantiate(Resources.Load<GameObject>("Prefabs/Sphere"));
+                blah.transform.position = this.transform.position;
+            }
+            if (coolDown > 0)
+            {
+                coolDown--;
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+}
